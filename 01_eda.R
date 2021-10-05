f87f9ff33a3231ab8b2dc8b0d1c026eeb2d6d464
J.F. Monteagudo <75076856+jfmont@users.noreply.github.com>
2021-10-05 11:29:49 +0200

    Add files via upload

--- 01_eda.R
+++ 01_eda.R
@@ -8,9 +8,9 @@ pacman::p_load(matrixstats)
 
 state <- read.csv("data/state.csv")
 
-##########################################################
-#### Location stimates of Population and Murder rates ####
-##########################################################
+###########################################################
+#### Location Estimates of Population and Murder rates ####
+###########################################################
 
 # Mean, trimmed mean and median 
 
@@ -26,3 +26,48 @@ weighted.mean(state$Murder.Rate, w=state$Population)
 
 matrixStats::weightedMedian(state$Murder.Rate, w=state$Population)
 
+###########################################################
+###### Variability Estimates of State Population ##########
+###########################################################
+
+# Sd, IQR, MAD
+
+sd(state$Population)
+
+IQR(state$Population)
+
+mad(state$Population)
+
+#As we can see, the sd is twice the mad (MAD is adjusted to be on the same scale as the mean), therefore MAD is more robust than sd. 
+
+###########################################################
+########## Exploring the Data Distribution ################
+###########################################################
+
+# Percentiles and Boxplots
+
+quantile(state$Murder.Rate, p = c(.05, .25, .50, .75, .95))
+
+boxplot(state$Population / 1000000, ylab = "Population (millions")
+
+# Frecuency Tables and Histograms
+
+breaks <- seq(
+  from = min(state$Population),
+  to = max(state$Population), 
+  length = 11)
+
+pop_freq <- cut(
+  state$Population, 
+  breaks = breaks,
+  right = T, 
+  include.lowest = T)
+
+table(pop_freq)
+
+# Histogram
+
+hist(state$Population, breaks = breaks, xlab = "Population", main = NULL)
+
+
+       
