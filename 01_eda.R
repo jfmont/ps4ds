cd02b982fc2f9ec777e244640b998cb217b5bbe0
J.F. Monteagudo <75076856+jfmont@users.noreply.github.com>
2021-10-05 13:11:21 +0200

    Add files via upload

--- 01_eda.R
+++ 01_eda.R
@@ -2,11 +2,13 @@
 
 #### Packages ####
 
-pacman::p_load(matrixstats)
+pacman::p_load(matrixstats, tidyverse)
 
 #### Data ####
 
-state <- read.csv("data/state.csv")
+urlfile <-  "https://raw.githubusercontent.com/gedeck/practical-statistics-for-data-scientists/master/data/state.csv"
+
+state <- read.csv(urlfile)
 
 ###########################################################
 #### Location Estimates of Population and Murder rates ####
@@ -44,13 +46,13 @@ mad(state$Population)
 ########## Exploring the Data Distribution ################
 ###########################################################
 
-# Percentiles and Boxplots
+# Percentiles and Box plots
 
 quantile(state$Murder.Rate, p = c(.05, .25, .50, .75, .95))
 
 boxplot(state$Population / 1000000, ylab = "Population (millions")
 
-# Frecuency Tables and Histograms
+# Frequency Tables and Histograms
 
 breaks <- seq(
   from = min(state$Population),
@@ -69,5 +71,29 @@ table(pop_freq)
 
 hist(state$Population, breaks = breaks, xlab = "Population", main = NULL)
 
+# Density Plots and Estimates
 
-       
+hist(state$Murder.Rate, freq = F, xlab = "Murder Rate (per 100,000)", ylab = "Density",  main = NULL) #Note freq = F, therefore histogram is plotted as a
+lines(density(state$Murder.Rate), lwd = 3, col = "blue")
+
+# Tidyverse approach
+
+ggplot(state, aes(x = Murder.Rate)) +
+  geom_histogram(aes(y=..density..), colour="black", fill="lightgrey", bins = 11) +
+  geom_density(color = "blue", size = 1.5) +
+  scale_x_continuous(limits = c(0,11), breaks = seq(from= 0, to = 10, by = 2)) +
+  scale_y_continuous(breaks = seq(from= 0., to= 0.20, by= 0.05)) +
+  xlab("Murder Rate (per 100,000)") +
+  ylab("Density") +
+  labs(title = NULL) +
+  theme_minimal()
+
+###########################################################
+#########  Exploring binary and categorical data###########
+###########################################################
+
+urlfile2 <- "https://raw.githubusercontent.com/gedeck/practical-statistics-for-data-scientists/master/data/dfw_airline.csv"
+
+dfw <- read.csv(urlfile2)
+
+
